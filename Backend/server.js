ed64c5f2c4c90642b5be9241a505d4a5f2f47477
marsaiba <mareedu.saibabu@publicissapient.com>
2022-05-17 17:50:53 +0530

    latest-changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-const staticFiles = path.join(__dirname, '../frontend/views');
+const staticFiles = path.join(__dirname, '../frontend');
 
 const frontPath = app.use(express.static(staticFiles));
 
 
 app.get('/', (req, res) => {
-    res.send(path.join(frontPath, 'index.html'))
+    res.send(path.join(frontPath, 'index.html'));
 })
 
 app.listen(PORT, () => {
