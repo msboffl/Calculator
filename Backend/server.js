d369f1205ccc2ef8e2b23f9c046302b6d0f6ed51
marsaiba <mareedu.saibabu@publicissapient.com>
2022-05-17 16:51:24 +0530

    Backend-server setup

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+const staticFiles = path.join(__dirname, '../frontend/views');
+
+const frontPath = app.use(express.static(staticFiles));
+
+
+app.get('/', (req, res) => {
+    res.send(path.join(frontPath, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Serever running at PORT:${PORT}`);
+})
+
